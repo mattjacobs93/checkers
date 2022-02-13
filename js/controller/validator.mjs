e106158cda965d22dd189791c2f1e03e3cf2fe5a
mattjacobs93 <matthewjacobs73@yahoo.com>
2022-02-12 19:39:58 -0800

    finish move validator

--- js/controller/validator.mjs
+++ js/controller/validator.mjs
@@ -5,14 +5,14 @@ let board =  [
   [0,-1,0,-1,0,-1,0,-1],
   [-1,0,-1,0,-1,0,-1,0],
   [0,-1,0,-1,0,-1,0,-1],
+  [0,0,1,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
-  [0,0,0,-1,0,0,0,0],
   [1,0,1,0,1,0,1,0],
-  [0,1,0,1,0,1,0,1],
+  [0,1,0,1,0,0,0,1],
   [1,0,1,0,1,0,1,0],
-]
+];
 
-
+//console.log('board after init: ' ,board[2])
 class Move {
   skip
   board
@@ -42,8 +42,10 @@ class ValidMoves {
 
 
 
-function getValidMovesPlayer1Checker (location, board) {
-  let validMovesObj = new ValidMoves()
+function getValidMoves (location, board) {
+  //console.log('starting board get valid moves', board[7])
+  //let validMovesObj = new ValidMoves()
+  let validMoves = []
   // let topLeft = [location[0]-1,location[1]-1]
   // let topRight = [location[0] -1,location[1] + 1]
   // let bottomLeft = [location[0]-1,location[1]-1]
@@ -52,18 +54,23 @@ function getValidMovesPlayer1Checker (location, board) {
   //let skipLeft = [location[0] - 2, location[1] - 2]
 
 
- function checkImmediatelyAdjacent (startingLocation, possibleAdjacent, board, validMovesObj) {
-
+ function checkImmediatelyAdjacent (startingLocation, possibleAdjacent, board, validMoves) {
+  console.log('beginning of check immediately adjacent' ,board[2][1])
   
   try {
     if (board[possibleAdjacent[0]][possibleAdjacent[1]] === 0) {
-      let newBoard = [...board]
-      newBoard[possibleAdjacent[0]][possibleAdjacent[1]] = 1
-      newBoard[startingLocation[0]][startingLocation[1]] = 0
+      let newBoard = board.slice().map(el=>el.slice())
+      //console.log(newBoard == board)
+      //console.log('new board',newBoard[2][1])
+      //newBoard[2][1] = 1000
+      //console.log(board[2][1])
+      newBoard[possibleAdjacent[0]][possibleAdjacent[1]] = 1;
+      newBoard[startingLocation[0]][startingLocation[1]] = 0;
       let movesArray = [startingLocation, possibleAdjacent]
       let newMove = new Move(movesArray, newBoard)
       newMove.movesArray = newMove.movesArray[0]
-      validMovesObj.validMovesArray.push(newMove)
+      validMoves.push(newMove)
+      console.log('end of check immediately adjacent', board[2][1])
 
     }
    } catch {
@@ -79,7 +86,8 @@ function getValidMovesPlayer1Checker (location, board) {
 
       if (Math.abs(pieceValue) > 1 || (pieceValue === 1 && i === -1) || (pieceValue === -1 && i === 1)) {
         let possibleAdjacent = [location[0] + i, location[1] + j]
-        checkImmediatelyAdjacent(location, possibleAdjacent, board, validMovesObj)
+        console.log('hello' ,board[2][1])
+        checkImmediatelyAdjacent(location, possibleAdjacent, board, validMoves)
       }
 
     }
@@ -89,17 +97,19 @@ function getValidMovesPlayer1Checker (location, board) {
  //checkImmediatelyAdjacent(location, topLeft, board)
  //checkImmediatelyAdjacent(location, topRight, board)
 
- function skip(location, board, validMovesObj, numMoves, moveObj) {
+ function skip(location, board, validMoves, numMoves, moveObj) {
    //if numMoves = 0, don't add self to validMoves,
   //increment numMoves by 1 after each iteration
   //basically just don't add the starting position
 
   if (numMoves > 0) {
     moveObj.movesArray.push(location)
-    validMovesObj.validMovesArray.push(moveObj) 
+    validMoves.push(moveObj) 
   }
 
   let pieceValue = board[location[0]][location[1]] 
+  console.log('starting skip values',location[0],location[1], pieceValue)
+  
 
   for (let i = -2; i <= 2; i += 4) {
     for (let j = -2; j <= 2; j += 4) {
@@ -109,11 +119,13 @@ function getValidMovesPlayer1Checker (location, board) {
       
       try {
             let adjacentLocation = [location[0] + parseInt(i/2), location[1] + parseInt(j/2)]
+            //console.log(adjacentLocation)
             let adjacentValue = board[adjacentLocation[0]][adjacentLocation[1]]
             let skipLocation = [location[0] + i, location[1] + j]
             let skipLocationIsEmpty = (board[skipLocation[0]][skipLocation[1]] === 0) ? true : false
            // let skipValue = board[skipLocation[0]][skipLocation[1]]
             let adjacentIsOpponent = ((pieceValue > 0 && adjacentValue < 0) || (pieceValue < 0 && adjacentValue > 0)) ? true : false
+            //console.log(skipLocationIsEmpty, adjacentIsOpponent, pieceValue, adjacentValue)
 
             if (skipLocationIsEmpty && adjacentIsOpponent) {
               let boardCopy = [...board]
@@ -121,7 +133,8 @@ function getValidMovesPlayer1Checker (location, board) {
               boardCopy[skipLocation[0]][skipLocation[1]] = pieceValue
               boardCopy[adjacentLocation[0]][adjacentLocation[1]] = 0
               let moveObjCopy = moveObj.copy()
-              skip(skipLocation,boardCopy,validMovesObj,numMoves+1,moveObjCopy)
+              console.log('about to recursively skip')
+              skip(skipLocation,boardCopy,validMoves,numMoves+1,moveObjCopy)
             }
 
         }
@@ -136,23 +149,14 @@ function getValidMovesPlayer1Checker (location, board) {
 
  }
  
- 
- skip(location,board,validMovesObj,0,new Move(location,board))
+ console.log(board[2][1])
+ skip(location,board,validMoves,0,new Move(location,[...board]))
  
  //console.log(validMoves)
- return validMovesObj
+ return validMoves
 }
 
-
-function getValidMoves(location, board) {
-  let locationRow = location[0]
-  let locationCol = location[1]  
-  let valueAtLocation = board[locationRow][locationCol]
-
-  if (valueAtLocation === 1) return getValidMovesPlayer1Checker(location,board)
-} 
-
-
-let validMovesArray = getValidMoves([5,2],board)
+console.log(board[2][1])
+let validMovesArray = getValidMoves([2,1],board)
 console.log(validMovesArray)
 
