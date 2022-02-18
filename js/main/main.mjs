110184b6406bbbabc2f1c841817a9d8f03199ed5
mattjacobs93 <matthewjacobs73@yahoo.com>
2022-02-17 21:02:45 -0800

    add starting form

--- js/main/main.mjs
+++ js/main/main.mjs
@@ -155,6 +155,135 @@ function displayMenu () {
   menu.style.position = 'relative'
   menu.style.top = '16.5vh'
   console.log('hi',rectBoard)
+  let menuSign = document.createElement('p')
+  menuSign.textContent = 'Starting Menu'
+  menuSign.classList.add('menu-sign')
+  menu.appendChild(menuSign)
+
+  let chooseP1 = document.createElement('p')
+  chooseP1.textContent = 'Player One:'
+  chooseP1.classList.add('choose-player-1')
+  menu.appendChild(chooseP1)
+
+
+
+  let chooseP2 = document.createElement('p')
+  chooseP2.textContent = 'Player Two:'
+  chooseP2.classList.add('choose-player-2')
+  menu.appendChild(chooseP2)
+
+  let humanButton1 = document.createElement('button')
+  humanButton1.classList.add('difficulty-level')
+  humanButton1.classList.add('player-one')
+  humanButton1.id = 'humanButton1'
+  humanButton1.textContent = 'Human'
+  menu.appendChild(humanButton1)
+
+  let aiEasy1 = document.createElement('button')
+  aiEasy1.classList.add('difficulty-level')
+  aiEasy1.classList.add('player-one')
+  aiEasy1.id = 'aiEasyButton1'
+  aiEasy1.textContent = 'AI-Easy'
+  menu.appendChild(aiEasy1)
+
+  let aiMedium1 = document.createElement('button')
+  aiMedium1.classList.add('difficulty-level')
+  aiMedium1.classList.add('player-one')
+  aiMedium1.id = 'aiMediumButton1'
+  aiMedium1.textContent = 'AI-Medium'
+  menu.appendChild(aiMedium1)
+
+  let aiHard1 = document.createElement('button')
+  aiHard1.classList.add('difficulty-level')
+  aiHard1.classList.add('player-one')
+  aiHard1.id = 'aiHardButton1'
+  aiHard1.textContent = 'AI-Hard'
+  menu.appendChild(aiHard1)
+
+
+
+
+  let humanButton2 = document.createElement('button')
+  humanButton2.classList.add('difficulty-level')
+  humanButton2.classList.add('player-two')
+  humanButton2.id = 'humanButton2'
+  humanButton2.textContent = 'Human'
+  menu.appendChild(humanButton2)
+
+  let aiEasy2 = document.createElement('button')
+  aiEasy2.classList.add('difficulty-level')
+  aiEasy2.classList.add('player-two')
+  aiEasy2.id = 'aiEasyButton2'
+  aiEasy2.textContent = 'AI-Easy'
+  menu.appendChild(aiEasy2)
+
+  let aiMedium2 = document.createElement('button')
+  aiMedium2.classList.add('difficulty-level')
+  aiMedium2.classList.add('player-two')
+  aiMedium2.id = 'aiMediumButton2'
+  aiMedium2.textContent = 'AI-Medium'
+  menu.appendChild(aiMedium2)
+
+  let aiHard2 = document.createElement('button')
+  aiHard2.classList.add('difficulty-level')
+  aiHard2.classList.add('player-two')
+  aiHard2.id = 'aiHardButton2'
+  aiHard2.textContent = 'AI-Hard'
+  menu.appendChild(aiHard2)
+
+  let startButton = document.createElement('button')
+  startButton.classList.add('startButton')
+  startButton.id = 'startButton'
+  startButton.textContent = 'start'
+  startButton.disabled = true
+  menu.appendChild(startButton) 
+
+  let player1DifficultyChosen = false
+  let player2DifficultyChosen = false
+
+  let player1
+  let player2
+
+  const checkStartButton = () => {
+    if (player1DifficultyChosen && player2DifficultyChosen) startButton.disabled = false
+  }
+
+  const clickPlayer1 = (e)=>{
+    let difficultyButtons = document.querySelectorAll('.player-one')
+    difficultyButtons.forEach(button=>button.classList.remove('active-button'))
+    e.target.classList.add('active-button')
+    player1 = e.target.id
+    player1DifficultyChosen = true
+    checkStartButton()
+  }
+
+  const clickPlayer2 = (e)=>{
+    let difficultyButtons = document.querySelectorAll('.player-two')
+    difficultyButtons.forEach(button=>button.classList.remove('active-button'))
+    e.target.classList.add('active-button')
+    player2 = e.target.id
+    player2DifficultyChosen = true
+    checkStartButton()
+  }
+
+  humanButton1.addEventListener('click', clickPlayer1)
+  humanButton2.addEventListener('click',clickPlayer2)
+
+  aiEasy1.addEventListener('click',clickPlayer1)
+  aiMedium1.addEventListener('click',clickPlayer1)
+  aiHard1.addEventListener('click',clickPlayer1)
+
+
+  aiEasy2.addEventListener('click',clickPlayer2)
+  aiMedium2.addEventListener('click',clickPlayer2)
+  aiHard2.addEventListener('click',clickPlayer2)
+
+  startButton.addEventListener('click',(e) => {
+    menu.remove()
+    view.turnOn()
+    //console.log(player1, player2)
+  })
+
 }
 
 
