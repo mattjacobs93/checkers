b2afe1ffd168399d53b6cd3f70e38c60dcd65706
mattjacobs93 <matthewjacobs73@yahoo.com>
2022-02-18 01:08:18 -0800

    refactor main

--- js/main/main.mjs
+++ js/main/main.mjs
@@ -2,14 +2,14 @@
 /*-------------------------------- Imports --------------------------------*/
 
 import * as viewImport from "../view/view.mjs";
-import * as PlayerImport from "../model/player.mjs"
-import * as ModelImport from '../model/model.mjs'
+import * as playerImport from "../model/player.mjs"
+import * as modelImport from '../model/model.mjs'
 import * as controllerImport from "../controller/controller.mjs"
 
 
 /*-------------------------------- Constants --------------------------------*/
 
-const model = new ModelImport.Model()
+const model = new modelImport.Model()
 const view = new viewImport.View()
 const controller = new controllerImport.Controller()
 const player1Value = 1
@@ -36,8 +36,8 @@ function getPlayers (isAIPlayer1, isAIPlayer2,depth1=0,depth2=0) {
     return [player1,player2]
   }
 
-  let player1  = new PlayerImport.Player(player1Value,isAIPlayer1,depth1)
-  let player2 = new PlayerImport.Player(player2Value, isAIPlayer2,depth2)
+  let player1  = new playerImport.Player(player1Value,isAIPlayer1,depth1)
+  let player2 = new playerImport.Player(player2Value, isAIPlayer2,depth2)
   let players = setUpPlayers(player1,player2)
 
   return players
@@ -62,7 +62,7 @@ function initGame() {
   controller.setView(view)
   boardDisplay.addEventListener('click', (e)=>view.acceptBoardClick(e))
   players = getPlayers(false,true)
-  activePlayer = new PlayerImport.ActivePlayer()
+  activePlayer = new playerImport.ActivePlayer()
   activePlayer.setPlayers(players[0],players[1])
   controller.setActivePlayer(activePlayer)
   view.setBoardDiv(boardDisplay)
